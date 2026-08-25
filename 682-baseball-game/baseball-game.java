692a816550f87d863b3f5140ba705ac4f8e5a338
ravi-dutt <ravidutt2003@gmail.com>
2026-08-25 09:53:28 +0530

    Time: 3 ms (80.62%) | Memory: 43.2 MB (96.38%) - LeetSync

--- 682-baseball-game/baseball-game.java
+++ 682-baseball-game/baseball-game.java
@@ -1,7 +1,6 @@
 class Solution {
     public int calPoints(String[] operations) {
         Stack<Integer> stack = new Stack<>();
-
         for(String op : operations){
             if(op.equals("C")) stack.pop();
             else if(op.equals("D")) stack.push(stack.peek()*2);
