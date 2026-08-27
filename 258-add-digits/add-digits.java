7a49aa537be0cb6aa21df921fb7083d4caf1f37d
Ravi Dutt Chauhan <ravidutt2003@gmail.com>
2026-08-27 22:37:54 +0530

    Time: 1 ms (98.05%) | Memory: 42.8 MB (25.72%) - LeetSync

---
+++ 258-add-digits/add-digits.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int addDigits(int num) {
+     int sum=0;
+     while(num>9)
+     {
+        while(num>0)
+        {
+            int ld=num%10;
+            sum=sum+ld;
+            num/=10;
+        }
+        num=sum;
+        sum=0;
+     } 
+     return num;  
+    }
+}
