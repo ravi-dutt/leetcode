41681dbe7329ba519cf5daaad92df0e13d1a1d01
Ravi Dutt Chauhan <ravidutt2003@gmail.com>
2026-08-29 12:58:36 +0530

    Time: 10 ms (43.67%) | Memory: 46.9 MB (76.85%) - LeetSync

---
+++ 838-design-linked-list/design-linked-list.java
@@ -0,0 +1,90 @@
+public class ListNode
+{
+    int val;
+    ListNode next;
+    ListNode(int x)
+    {
+        val=x;
+    }
+}
+class MyLinkedList {
+    ListNode head;
+    int size;
+    public MyLinkedList() {
+         head=new ListNode(0);
+         size=0;
+    }
+    
+    public int get(int index) {
+        if(index<0)
+        {
+            return -1;
+        }
+        if(index>=size)
+        {
+            return -1;
+        }
+        ListNode curr=head;
+        for(int i=0;i<=index;i++)
+        {
+            curr=curr.next;
+        }
+        return curr.val;
+        
+    }
+    
+    public void addAtHead(int val) {
+        addAtIndex(0,val);
+    }
+    
+    public void addAtTail(int val) {
+        addAtIndex(size,val);
+    }
+    
+    public void addAtIndex(int index, int val) {
+        if(index<0)
+        {
+            return;
+        }
+        if(index>size)
+        {
+            return;
+        }
+        ListNode curr=head;
+          size++;
+        for(int i=0;i<index;i++)
+        {
+            curr=curr.next;
+        }
+        ListNode n=new ListNode(val);
+        n.next=curr.next;
+        curr.next=n;
+  }
+    public void deleteAtIndex(int index) {
+         if(index<0)
+        {
+            return;
+        }
+        if(index>=size)
+        {
+            return;
+        }
+        ListNode curr=head;
+        for(int i=0;i<index;i++)
+        {
+            curr=curr.next;
+        }
+         size--;
+        curr.next=curr.next.next;
+    }
+}
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.get(index);
+ * obj.addAtHead(val);
+ * obj.addAtTail(val);
+ * obj.addAtIndex(index,val);
+ * obj.deleteAtIndex(index);
+ */
