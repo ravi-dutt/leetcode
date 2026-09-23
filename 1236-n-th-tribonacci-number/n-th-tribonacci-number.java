b1d24b9b077a44b2810303a4b07257354a11eae7
Ravi Dutt Chauhan <ravidutt2003@gmail.com>
2026-09-23 11:05:16 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (52.78%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    Map<Integer,Integer>map=new HashMap<>();
+    public int tribonacci(int n) {
+        if(map.containsKey(n))
+        {
+            return map.get(n);
+        }
+        if(n==0)
+        {
+            return 0;
+        }
+        if(n==1||n==2)
+        {
+            return 1;
+        }
+        map.put(n,tribonacci(n-3)+tribonacci(n-2)+tribonacci(n-1));
+        return map.get(n);
+    }
+}
