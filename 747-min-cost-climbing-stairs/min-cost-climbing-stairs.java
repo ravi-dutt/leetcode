8a1aacfc1343f06521f80c20e9514c87edc0aba9
Ravi Dutt Chauhan <ravidutt2003@gmail.com>
2026-09-23 11:29:29 +0530

    Time: 4 ms (1.11%) | Memory: 46.5 MB (10.30%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -1,13 +1,20 @@
 class Solution {
-    public int minCostClimbingStairs(int[] cost) {
-        int []f=new int[cost.length+1];
-        f[0]=0;
-        f[1]=0;
-        for(int i=2;i<f.length;i++)
+    Map<Integer,Integer>map=new HashMap<>();
+    public int f(int n,int[]cost)
+    {
+        if(n<=1)
         {
-            f[i]=Math.min(cost[i-1]+f[i-1],
-            cost[i-2]+f[i-2]);
+            return 0;
         }
-        return f[cost.length];
+        if(map.containsKey(n))
+        {
+            return map.get(n);
+        }
+        map.put(n,Math.min(cost[n-1]+f(n-1,cost),
+                           cost[n-2]+f(n-2,cost)));
+        return map.get(n);
+    }
+    public int minCostClimbingStairs(int[] cost) {
+        return f(cost.length,cost);
     }
 }
